cebf4cafc52dd05f604336b2c911a54f56f7677e
Maikko Aleman <maikko.aleman@gmail.com>
2019-11-13 16:17:48 +0800

    Setup: prettier rules

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
-import React from 'react'
+import React from 'react';
 
-import '../styles/index.css'
+import '../styles/index.css';
 
 export default () => (
   <div className="p-4 shadow rounded bg-white">
     <h1 className="text-purple-500 leading-normal">Next.js</h1>
     <p className="text-gray-500">with Tailwind CSS</p>
   </div>
-)
+);
